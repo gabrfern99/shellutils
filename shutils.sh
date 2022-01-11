ec42a2aa6a5978f92865c22c2adbc2e28bb05e60
codx1 <gabr.fern99@gmail.com>
2022-01-11 02:06:20 -0300

    Add new function

--- shutils.sh
+++ shutils.sh
@@ -8,7 +8,7 @@
 # readed in the sh -c '...' part
 function OTNext()
 {
-	echo -n "Directory: "
+	echo -n "Full directory path: "
 	read -r dir
 	echo -n "From extension (zip, bz2, jpeg, ...): "
 	read -r frext
@@ -18,18 +18,45 @@ function OTNext()
 	find "$dir" -name "*.$frext" -exec sh -c "for f; do echo "$f" "${f%.*}.${toext}"; done" {} +
 
 }
+# Function that cut away part of a long file name
+# and preserves extension
+function cut_long_fname()
+{
+	echo -n "Full directory path: "
+	read -r dir
+
+	find "$dir" -type f > /tmp/tmpfnames
+	n=0
+	while read -r fname; do
+		fext="${fname##*.}"
+		if [ "${#fname}" -gt 244 ]; then
+			n=$((n+1))
+			nfname=${fname:0:240}
+			mv "$fname" "$nfname.$fext"
+		else
+			continue
+		fi
+	done < /tmp/tmpfnames
+
+	echo ''
+	echo "$n items renamed"
+	rm /tmp/tmpfnames
+}
 
 echo "Function description - Function Name"
 echo "Old ext to new ext - OTNext"
+echo "Cut long file names - cut_long_fname"
 echo ""
 
 if [ $# -ne 1 ]; then
 	echo "Usage: shutils.sh funcname"
 	exit
-	fi
+fi
 
 if [[ $1 == "OTNext" ]]; then
 	OTNext
+elif [[ $1 == "cut_long_fname" ]]; then
+	cut_long_fname
 else
 	echo "No function with that name"
 	exit
